512a5b86791de65bf110304f49fd7d6eaf348e41
Darko <dj_mamlyooo@hotmail.com>
2023-10-31 12:48:29 +0100

    userRoutes imported and ready to use on /api/users

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const MONGO_URI = process.env.MONGO_URI;
@@ -11,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/users", userRoutes);
 
 //database connection
 mongoose
