a0015b7782e739ba56a0481c9b247ea9ed52b544
Darko <dj_mamlyooo@hotmail.com>
2023-11-01 23:59:14 +0100

    songController uploadSong function done!

--- backend/controllers/songController.js
+++ backend/controllers/songController.js
@@ -2,16 +2,37 @@ import express from "express";
 import Song from "../models/song";
 import multer from "multer";
 
-//Setpu storage using muslter
+//Setup storage using multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./uploads/");
   },
-  finalname: function (req, file, cb) {
+  filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
 const upload = multer({ storage: storage });
 
+//Upload a song
+export const uploadSong = async (req, res) => {
+  try {
+    const { title, artist, duration } = req.body;
 
+    //Create a new song instance
+    const song = new Song({
+      title,
+      artist,
+      duration,
+      url: req.file.path, //local path to the uploaded file
+    });
+
+    await sessionStorage.save();
+    res.status(201).json({ message: "Song uploaded successfully", song });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+//Use multer middleware for file handling
+router.post("/upload", upload.single("songFile"), uploadSong);
