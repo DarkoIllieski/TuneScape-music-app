bd3d9d64f27a7907dc91b1390e6c7826ac1b46bf
Darko <dj_mamlyooo@hotmail.com>
2023-11-01 23:45:32 +0100

    SongSchema metadata updated

--- backend/models/song.js
+++ backend/models/song.js
@@ -4,6 +4,9 @@ const songSchema = new mongoose.Schema({
   title: { type: String, required: true },
   artist: { type: String, required: true },
   price: { type: String, required: true },
+  duration: { type: Number },
+  url: { type: String, required: true },
+  converIMgae: { type: String },
 });
 
 const Song = mongoose.model("Song", songShema);
