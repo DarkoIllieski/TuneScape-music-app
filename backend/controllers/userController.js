0383a386d1425b7adcbf497f774a951adfa8301d
Darko <dj_mamlyooo@hotmail.com>
2023-11-07 23:56:05 +0100

    deleteUser added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,4 +44,22 @@ export const loginUser = async (req, res) => {
   }
 };
 
+import User from "../models/user.js";
+
+export const deleteUser = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+
+    const deletedUser = await User.findByIdAndDelete(userId);
+
+    if (deletedUser) {
+      res.status(200).json({ message: "User deleted successfully" });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
 
