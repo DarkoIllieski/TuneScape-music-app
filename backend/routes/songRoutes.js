539fba6e61d0718b629a07c5f3c2c64cf5d3d1e8
Darko <dj_mamlyooo@hotmail.com>
2023-11-07 23:46:40 +0100

    deleteSong route created

--- backend/routes/songRoutes.js
+++ backend/routes/songRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { uploadSong } from "../controllers/songController";
 import multer from "multer";
+import { getAllSongs, searchTracks, uploadSong } from "../controllers/songController.js";
 
 const router = express.Router();
 const storage = multer.memoryStorage();
@@ -10,5 +11,9 @@ router.post("/upload", upload.single('songFile'), uploadSong)
 
 router.get("/songs", getAllSongs)
 router.get('/search', searchTracks)
+
+//delete song by id
+router.delete("//songs/:id", deleteSong);
+
 
 export default router
