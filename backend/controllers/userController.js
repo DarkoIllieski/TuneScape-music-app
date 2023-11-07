2b40fec84bcf225a2e4f19e3a76117bca7006edc
Darko <dj_mamlyooo@hotmail.com>
2023-11-07 23:58:38 +0100

    updateUser it' in the game

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,3 +63,24 @@ export const deleteUser = async (req, res) => {
   }
 };
 
+import User from "../models/user.js";
+
+export const updateUser = async (req, res) => {
+  const { userId } = req.params;
+  const updatedUserData = req.body;
+
+  try {
+
+    const updatedUser = await User.findByIdAndUpdate(userId, updatedUserData, { new: true });
+
+    if (updatedUser) {
+      res.status(200).json({ message: "User updated successfully", user: updatedUser });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+
