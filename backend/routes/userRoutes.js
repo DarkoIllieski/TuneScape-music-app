e00381d66e08a7cb759ff4af7a08935d4c956201
Darko <dj_mamlyooo@hotmail.com>
2023-11-07 23:57:12 +0100

    deleteUser route in

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/userController.js";
+import { registerUser, loginUser, deleteUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 // Register User
 router.post("/register", registerUser);
+// Login User
 router.post("/login", loginUser);
+
+//de;ete user by id
+router.delete("/users/:userId", deleteUser);
 
 export default router;
