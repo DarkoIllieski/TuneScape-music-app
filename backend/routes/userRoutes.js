6407c097449cc79e2ffa805d9c1addcbcad65f2f
Darko <dj_mamlyooo@hotmail.com>
2023-11-07 23:59:34 +0100

    updateUser route in

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,7 +8,11 @@ router.post("/register", registerUser);
 // Login User
 router.post("/login", loginUser);
 
+router.put("/users/:userId", updateUser);
+
 //de;ete user by id
 router.delete("/users/:userId", deleteUser);
+
+
 
 export default router;
