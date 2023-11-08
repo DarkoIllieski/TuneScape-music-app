19be18ebf8355727084358f6f67fe8d197897fc0
Darko <dj_mamlyooo@hotmail.com>
2023-11-09 00:08:50 +0100

    getSongById controller ready

--- backend/controllers/songController.js
+++ backend/controllers/songController.js
@@ -5,52 +5,6 @@ import dotenv from "dotenv";
 import Song from "../models/song.js";
 
 dotenv.config();
-
-//Setup storage using multer
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
-
-const router = express.Router();
-
-//Upload a song
-export const uploadSong = async (req, res) => {
-  try {
-    const { title, artist, duration } = req.body;
-
-    //Create a new song instance
-    const song = new Song({
-      title,
-      artist,
-      duration,
-      url: req.file.path, //local path to the uploaded file
-    });
-
-    await sessionStorage.save();
-    res.status(201).json({ message: "Song uploaded successfully", song });
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-};
-
-//Get all songs
-
-export const getAllSongs = async (req, res) => {
-  try {
-    const songs = await Song.find();
-    res.status(200).json(songs);
-  } catch {
-    res.status(500).send(error.message);
-  }
-};
-
 const LASTFM_API_KEY = process.env.LASTFM_API_KEY;
 
 export const searchTracks = async (req, res) => {
@@ -71,6 +25,51 @@ export const searchTracks = async (req, res) => {
   }
 };
 
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const router = express.Router();
+
+export const uploadSong = async (req, res) => {
+  try {
+    const { title, artist, duration } = req.body;
+
+
+    const song = new Song({
+      title,
+      artist,
+      duration,
+      url: req.file.path, 
+    });
+
+    await sessionStorage.save();
+    res.status(201).json({ message: "Song uploaded successfully", song });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+//Get all songs
+
+export const getAllSongs = async (req, res) => {
+  try {
+    const songs = await Song.find();
+    res.status(200).json(songs);
+  } catch {
+    res.status(500).send(error.message);
+  }
+};
+
+
 import Song from "../models/song.js";
 
 export const deleteSong = async (req, res) => {
@@ -89,4 +88,18 @@ export const deleteSong = async (req, res) => {
     res.status(500).send(error.message);
   }
 };
+
+export const getSongById = async (req, res) => {
+  try {
+    const { songId } = req.params;
+    const song = await Song.findById(songId);
+    if (!song) {
+      res.status(404).json({ message: "Song not found" });
+    } 
+    res.status(200).json(song);
+  }catch (error) {
+    res.status(500).send(error.message);
+  } 
+
+}
 
