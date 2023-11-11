e4fbf75949ced80b16ae346569b1d42853b12c14
Darko <dj_mamlyooo@hotmail.com>
2023-11-11 23:51:59 +0100

    homePage initialized

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
+// App.jsx
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
 
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-  
-    </>
-  )
-}
+    <Router>
+      <div className="app-container">
+        <Header />
 
-export default App
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+        </Switch>
+
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
