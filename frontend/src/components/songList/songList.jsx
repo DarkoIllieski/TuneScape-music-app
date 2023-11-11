481392c0cb51e9c3d8e4832356fa67bcac76b0e9
Darko <dj_mamlyooo@hotmail.com>
2023-11-11 23:59:07 +0100

    songList functionality  modified

--- frontend/src/components/songList/songList.jsx
+++ frontend/src/components/songList/songList.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
 
-const songList = () => {
+import React from 'react';
+
+const SongList = ({ songs }) => {
   return (
-    <div>songList</div>
-  )
-}
+    <div className="song-list">
+      <h2>Song List</h2>
+      <ul>
+        {songs.map((song) => (
+          <li key={song.id}>
+            <div>
+              <strong>{song.title}</strong> - {song.artist}
+            </div>
+            {/* Add more details or controls for each song */}
+            <button onClick={() => handlePlay(song)}>Play</button>
+            <button onClick={() => handleDelete(song.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default songList
+export default SongList;
