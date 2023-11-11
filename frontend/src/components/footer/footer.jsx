c6c66a4b9cbd3f8db5f76a978cc8b84c48a2e53e
Darko <dj_mamlyooo@hotmail.com>
2023-11-11 23:57:58 +0100

    songList.jsx has been created

---
+++ frontend/src/components/footer/footer.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-content">
+        <p>&copy; TuneScape</p>
+        <nav>
+          <a href="/terms">Terms of Service</a>
+          <a href="/privacy">Privacy Policy</a>
+          <a href="/contact">Contact Us</a>
+        </nav>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/components/songList/songList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const songList = () => {
+  return (
+    <div>songList</div>
+  )
+}
+
+export default songList
