df92e2886330ff569b490ba872627c4dbbded20b
Darko <dj_mamlyooo@hotmail.com>
2023-11-12 23:07:37 +0100

    userRegister logic ready

---
+++ frontend/src/components/userRegister/userRegister.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted:', formData);
+    setFormData({
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    });
+  };
+
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Register;
