574b403b90f8516700a446e8b70bf4bdb3da73a2
Darko <dj_mamlyooo@hotmail.com>
2023-11-23 21:04:42 +0100

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,14 @@ const MONGO_URI = process.env.MONGO_URI;
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/users", userRoutes);
